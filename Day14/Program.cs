567579a428a6bb978da18a27f49602845731ce9a
pmaga <pawel.p.maga@gmail.com>
2023-12-17 18:26:57 +0100

    day14

---
+++ Day14/Program.cs
@@ -0,0 +1,181 @@
+﻿var data = new List<char[]>();
+
+foreach (var line in await File.ReadAllLinesAsync("./input.txt"))
+{
+    data.Add(line.ToCharArray());
+}
+
+List<string> history = new();
+
+var n = 1000000000;
+for (var i = 0; i < n; i++)
+{
+   OneCycle();
+
+   var state = string.Join('\n', data.Select(x => new string(x)));
+
+   var index = history.IndexOf(state);
+   if (index >= 0)
+   {
+       var match = (n - index) % (history.Count - index) + index;
+       data = history[match - 1].Split('\n').Select(x => x.ToCharArray()).ToList();
+       break;
+   }
+
+   history.Add(state);
+}
+
+//Draw();
+
+Console.WriteLine($"Points: {GetPoints()}");
+
+void OneCycle()
+{
+    //Draw(); Console.WriteLine();
+    
+    RollNorth();
+    //Draw(); Console.WriteLine();
+    
+    RollLeft();
+    //Draw(); Console.WriteLine();
+    
+    RollSouth();
+    //Draw(); Console.WriteLine();
+
+    RollRight();
+    //Draw(); Console.WriteLine();
+}
+
+bool RollNorth()
+{
+    var movedAny = false;
+    for (var currentCharIndex = 0; currentCharIndex < data[0].Length; currentCharIndex++)
+    {
+        for (var currentRowIndex = 1; currentRowIndex < data.Count; currentRowIndex++)
+        {
+            for (var parentRowIndex = currentRowIndex - 1; parentRowIndex >= 0; parentRowIndex--)
+            {
+                var currentRow = data[currentRowIndex];
+                var parentRow = data[parentRowIndex];
+
+                if (parentRow[currentCharIndex] == '.' && currentRow[currentCharIndex] == 'O')
+                {
+                    parentRow[currentCharIndex] = 'O';
+                    currentRow[currentCharIndex] = '.';
+                    movedAny = true;
+                    currentRowIndex--;
+                }
+                else
+                {
+                    parentRowIndex = 0; //end
+                }
+            }
+        }
+    }
+    return movedAny;
+}
+
+bool RollSouth()
+{
+    var movedAny = false;
+    for (var currentCharIndex = 0; currentCharIndex < data![0].Length; currentCharIndex++)
+    {
+        for (var currentRowIndex = data.Count - 2; currentRowIndex >= 0; currentRowIndex--)
+        {
+            for (var parentRowIndex = currentRowIndex + 1; parentRowIndex < data.Count; parentRowIndex++)
+            {
+                var currentRow = data[currentRowIndex];
+                var parentRow = data[parentRowIndex];
+
+                if (parentRow[currentCharIndex] == '.' && currentRow[currentCharIndex] == 'O')
+                {
+                    parentRow[currentCharIndex] = 'O';
+                    currentRow[currentCharIndex] = '.';
+                    movedAny = true;
+                    currentRowIndex++;
+                }
+                else
+                {
+                    parentRowIndex = data.Count; //end
+                }
+            }
+        }
+    }
+    return movedAny;
+}
+
+bool RollRight()
+{
+    var movedAny = false;
+    for (var currentRowIndex = 0; currentRowIndex < data.Count; currentRowIndex++)
+    {
+        for (var currentCharIndex = 0; currentCharIndex < data[0].Length; currentCharIndex++)
+        {
+            for (var leftCharIndex = currentCharIndex - 1; leftCharIndex >= 0; leftCharIndex--)
+            {
+                var currentRow = data[currentRowIndex];
+
+                if (currentRow[currentCharIndex] == '.' && currentRow[leftCharIndex] == 'O')
+                {
+                    currentRow[currentCharIndex] = 'O';
+                    currentRow[leftCharIndex] = '.';
+                    movedAny = true;
+                    currentCharIndex--;
+                }
+                else
+                {
+                    leftCharIndex = 0; //end
+                }
+            }
+        }
+    }
+    return movedAny;
+}
+
+bool RollLeft()
+{
+    var movedAny = false;
+    for (var currentRowIndex = 0; currentRowIndex < data.Count; currentRowIndex++)
+    {
+        for (var currentCharIndex = 0; currentCharIndex < data[0].Length - 1; currentCharIndex++)
+        {
+            var currentRow = data[currentRowIndex];
+
+            if (currentRow[currentCharIndex] == '.' && currentRow[currentCharIndex + 1] == 'O')
+            {
+                currentRow[currentCharIndex] = 'O';
+                currentRow[currentCharIndex + 1] = '.';
+                movedAny = true;
+                
+                currentCharIndex = -1;
+            }
+        }
+    }
+
+    return movedAny;
+}
+
+void Draw()
+{
+    foreach (var d in data)
+    {
+        Console.WriteLine(string.Join(string.Empty, d));
+    }
+}
+
+int GetPoints()
+{
+    var points = 0;
+    for (var i = 0; i < data.Count; i++)
+    {
+        for (var j = 0; j < data[i].Length; j++)
+        {
+            if (data[i][j] == 'O')
+            {
+                points += data.Count - i;
+            }
+        }
+    }
+
+    return points;
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,142 @@
+﻿using System.Numerics;
+
+var points = 0;
+
+// Part 1
+var lines = await File.ReadAllLinesAsync("./input.txt");
+
+var seedToSoilMap = new List<RangeMap>();
+var soilToFertilizerMap = new List<RangeMap>();
+var fertilizerToWaterMap = new List<RangeMap>();
+var waterToLightMap = new List<RangeMap>();
+var lightToTemperatureMap = new List<RangeMap>();
+var temperatureToHumidityMap = new List<RangeMap>();
+var humidityToLocationMap = new List<RangeMap>();
+
+var seeds = lines[0][7..].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+
+for (var index = 1; index < lines.Length; index++)
+{
+    var line = lines[index];
+    switch (line)
+    {
+        case "seed-to-soil map:":
+            seedToSoilMap = GetAllValues();
+            continue;
+        case "soil-to-fertilizer map:":
+            soilToFertilizerMap = GetAllValues();
+            continue;
+        case "fertilizer-to-water map:":
+            fertilizerToWaterMap = GetAllValues();
+            continue;
+        case "water-to-light map:":
+            waterToLightMap = GetAllValues();
+            continue;
+        case "light-to-temperature map:":
+            lightToTemperatureMap = GetAllValues();
+            continue;
+        case "temperature-to-humidity map:":
+            temperatureToHumidityMap = GetAllValues();
+            continue;
+        case "humidity-to-location map:":
+            humidityToLocationMap = GetAllValues();
+            continue;
+    }
+
+    continue;
+
+    List<RangeMap> GetAllValues()
+    {
+        index++;
+
+        var arr = new List<RangeMap>();
+        while (!string.IsNullOrWhiteSpace(line) && index < lines.Length)
+        {
+            line = lines[index];
+            index++;
+            
+            var s = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (s.Length == 3)
+            {
+                var range = new RangeMap(long.Parse(s[0]), long.Parse(s[1]), long.Parse(s[2]));
+                arr.Add(range);
+            }
+            else
+            {
+                index--;
+            }
+        }
+
+        return arr;
+    }
+}
+
+// part 1
+// var locations = new List<long>();
+// for (var i = 0; i < seeds.Count; i += 2)
+// {
+//     var seed = seeds[i];
+//     
+//     var soilMatch = seedToSoilMap.SingleOrDefault(x => seed >= x.SourceStart && seed <= x.SourceStart + x.Range - 1);
+//     var soil = soilMatch is null ? seed : seed + Diff(soilMatch.DestinationStart, soilMatch.SourceStart);
+//
+//     var fertilizerMatch = soilToFertilizerMap.SingleOrDefault(x => soil >= x.SourceStart && soil <= x.SourceStart + x.Range - 1);
+//     var fertilizer = fertilizerMatch is null ? soil : soil + Diff(fertilizerMatch.DestinationStart, fertilizerMatch.SourceStart);
+//
+//     var waterMatch = fertilizerToWaterMap.SingleOrDefault(x => fertilizer >= x.SourceStart && fertilizer <= x.SourceStart + x.Range - 1);
+//     var water = waterMatch is null ? fertilizer : fertilizer + Diff(waterMatch.DestinationStart, waterMatch.SourceStart);
+//
+//     var lightMatch = waterToLightMap.SingleOrDefault(x => water >= x.SourceStart && water <= x.SourceStart + x.Range - 1);
+//     var light = lightMatch is null ? water : water + Diff(lightMatch.DestinationStart, lightMatch.SourceStart);
+//
+//     var temperatureMatch = lightToTemperatureMap.SingleOrDefault(x => light >= x.SourceStart && light <= x.SourceStart + x.Range - 1);
+//     var temperature = temperatureMatch is null ? light : light + Diff(temperatureMatch.DestinationStart, temperatureMatch.SourceStart);
+//
+//     var humidityMatch = temperatureToHumidityMap.SingleOrDefault(x => temperature >= x.SourceStart && temperature <= x.SourceStart + x.Range - 1);
+//     var humidity = humidityMatch is null ? temperature : temperature + Diff(humidityMatch.DestinationStart, humidityMatch.SourceStart);
+//
+//     var locationMatch = humidityToLocationMap.SingleOrDefault(x => humidity >= x.SourceStart && humidity <= x.SourceStart + x.Range - 1);
+//     var location = locationMatch is null ? humidity : humidity + Diff(locationMatch.DestinationStart, locationMatch.SourceStart);
+//     locations.Add(location);
+//     
+//     long Diff(long x, long y) => x - y;
+// }
+
+// part2
+var locations = new List<long>();
+for (var i = 0; i < seeds.Count; i += 2)
+{
+    for (var j = 0; j < seeds[i + 1]; j++)
+    {
+        var seed = seeds[i];
+        
+        var soilMatch = seedToSoilMap.SingleOrDefault(x => seed >= x.SourceStart && seed <= x.SourceStart + x.Range - 1);
+        var soil = soilMatch is null ? seed : seed + Diff(soilMatch.DestinationStart, soilMatch.SourceStart);
+
+        var fertilizerMatch = soilToFertilizerMap.SingleOrDefault(x => soil >= x.SourceStart && soil <= x.SourceStart + x.Range - 1);
+        var fertilizer = fertilizerMatch is null ? soil : soil + Diff(fertilizerMatch.DestinationStart, fertilizerMatch.SourceStart);
+    
+        var waterMatch = fertilizerToWaterMap.SingleOrDefault(x => fertilizer >= x.SourceStart && fertilizer <= x.SourceStart + x.Range - 1);
+        var water = waterMatch is null ? fertilizer : fertilizer + Diff(waterMatch.DestinationStart, waterMatch.SourceStart);
+    
+        var lightMatch = waterToLightMap.SingleOrDefault(x => water >= x.SourceStart && water <= x.SourceStart + x.Range - 1);
+        var light = lightMatch is null ? water : water + Diff(lightMatch.DestinationStart, lightMatch.SourceStart);
+    
+        var temperatureMatch = lightToTemperatureMap.SingleOrDefault(x => light >= x.SourceStart && light <= x.SourceStart + x.Range - 1);
+        var temperature = temperatureMatch is null ? light : light + Diff(temperatureMatch.DestinationStart, temperatureMatch.SourceStart);
+    
+        var humidityMatch = temperatureToHumidityMap.SingleOrDefault(x => temperature >= x.SourceStart && temperature <= x.SourceStart + x.Range - 1);
+        var humidity = humidityMatch is null ? temperature : temperature + Diff(humidityMatch.DestinationStart, humidityMatch.SourceStart);
+    
+        var locationMatch = humidityToLocationMap.SingleOrDefault(x => humidity >= x.SourceStart && humidity <= x.SourceStart + x.Range - 1);
+        var location = locationMatch is null ? humidity : humidity + Diff(locationMatch.DestinationStart, locationMatch.SourceStart);
+        locations.Add(location);
+    }
+    
+    long Diff(long x, long y) => x - y;
+}
+
+
+Console.WriteLine($"Min: {locations.Min()}");
+
+public sealed record RangeMap(long DestinationStart, long SourceStart, long Range);
